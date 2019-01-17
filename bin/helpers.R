3a9aaec41021f6c1d54152939dc8ca7807ffefce
MKLau <matthewklau@fas.harvard.edu>
2019-01-17 18:42:30 -0500

    adding conditional probability

--- bin/helpers.R
+++ bin/helpers.R
@@ -164,7 +164,38 @@ freqNet <- function(x, zero.diag = FALSE){
     x
 }
 
-coNets <- function(x, ci.p = 95, scale = FALSE, return.signs = FALSE){
+cond_prob <- function(a, b){
+    ## P(B) = B / N
+    ## P(A) = A / N
+    ## P(A,B) = AB / N 
+    ## P(B|A) = P(A,B) / P(A)
+    ## P(A|B) = P(A,B) / P(B)
+    n <- length(a)
+    p.a <- sum(a) / n
+    p.b <- sum(b) / n
+    p.ab <- sum(sign(a + b == 2)) / n
+    p.a_b <- p.ab / p.b
+    p.b_a <- p.ab / p.a
+    return(c(p.a_b, p.b_a))
+}
+
+cond_net <- function(x){
+    out <- matrix(0, nrow = ncol(x), ncol = ncol(x))
+    rownames(out) <- colnames(out) <- colnames(x)
+    for (i in 1:ncol(x)){
+        for (j in i:ncol(x)){
+            cp <- cond_prob(sign(x[, i]), sign(x[, j]))
+            ##This makes P(row | col)
+            out[j, i] <- cp[1]
+            out[i, j] <- cp[2]
+        }
+    }
+    out[is.na(out)] <- 0
+    return(out)
+}
+
+coNets <- function(x, conditional = FALSE, 
+                   ci.p = 95, scale = FALSE, return.signs = FALSE){
     Z.tab <- c("80" = 1.282, "85" = 1.440, "90" = 1.645, "95" = 1.960, "99" = 2.576)
     Z <- Z.tab[as.character(floor(ci.p))]
     if (class(x) == "data.frame"){x <- as.matrix(x)}
@@ -185,6 +216,9 @@ coNets <- function(x, ci.p = 95, scale = FALSE, return.signs = FALSE){
     if (return.signs){
         net[ab > ci.u] <- net[ab > ci.u] * 1
         net[ab < ci.l] <- net[ab < ci.l] * -1
+    }
+    if (conditional){
+        net <- cond_net(x) * sign(net)
     }
     return(net)
 }
