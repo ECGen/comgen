9f05f81272fc0e4e1485dac44958703b172a90ed
mklau <glomus@gmail.com>
2020-05-04 21:10:35 +0000

    Removes discussion text from scratch.R

--- R/scratch.R
+++ R/scratch.R
@@ -1,10 +1,3 @@
-## Results
-
-## Genotype affects networks via bark roughness
-
-## Lichen community variation, which did not respond to genotype or
-## bark roughness, correlated with lichen network structure
-
 
 cor.test(onc.com[,"Xg"], BR)
 cor.test(onc.com[,"Xg"], L)
