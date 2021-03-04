874379d1cb002c7aad749dd4df30c2632f345d21
mklau <mk@mklau.info>
2021-03-04 09:43:34 -0500

    removes old files in R directory

--- R/scratch.R
+++
@@ -1,9 +0,0 @@
-
-
-###
-cor.test(onc.com[,"Xg"], BR)
-cor.test(onc.com[,"Xg"], L)
-cor.test(onc.com[,"Xg"], Cen)
-cor.test(onc.com[,"Xg"], AMI)
-round(cor(cbind(onc.dat[, c("BR", "CT", "pH", "CN")], onc.com[, 1:5])), 3)[1:4,5:9]
-round(cor(cbind(onc.com[, 1:5], onc.dat[, c("PC", "SR", "SD", "L", "Cen", "AMI")])), 3)[1:5,6:11]
