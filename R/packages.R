20d096ca6f30e8a64da367f63ac7d49f85798645
MKLau <MKLau3@hawaii.edu>
2026-05-30 01:59:55 +0000

    Removes both option

--- R/packages.R
+++ R/packages.R
@@ -1,5 +1,4 @@
 ## Check for supporting packages
-options(pkgType = "both")     # try binary, then source if needed.
 cran.pkgs <- c("magrittr", "devtools", "beepr",
               "xtable", "reshape", "tools",
               "MuMIn", "lme4", "RLRsim", "vegan", "ecodist", "igraph",
