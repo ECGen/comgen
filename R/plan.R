2704d979d1bf669c1fb8e3a5032231d18a59a1dc
MKLau <MKLau@users.noreply.github.com>
2026-06-05 18:32:28 -1000

    Adds rendering of supporting information to plan.R

--- R/plan.R
+++ R/plan.R
@@ -199,4 +199,10 @@ plan <- drake_plan(
         dir = "docs/lcn_manuscript", 
         file.tex = "main.tex",
         render = FALSE)
+### Generate Supporting Information
+    update.manuscript = update_manuscript(
+        files = tables_figures, 
+        dir = "docs/lcn_manuscript", 
+        file.tex = "supporting_information.tex",
+        render = FALSE)
 )
